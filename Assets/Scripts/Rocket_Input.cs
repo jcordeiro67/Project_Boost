9ac528c2d2f4157e519f92b7ecb0a93775f30d85
John Cordeiro <john@johncordeiro.com>
2018-02-02 17:17:19 -0500

    Player movement fixed, collider and trigger detection by tag added, basic player destruction added.

--- Assets/Scripts/Rocket_Input.cs
+++ Assets/Scripts/Rocket_Input.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 using System;
 
-public class Rocket_Input : MonoBehaviour {
+// Required unity components, missing components will be added to the gameobject when this scipt is attached.
+using UnityEngine.Timeline;
 
-	public float ThrustForce = 20f;
-	public float RotationSpeed = 5f;
+
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+
+public class Rocket_Input : MonoBehaviour {
+	[Header("Physics Forces")]
+	[SerializeField] float mainThrust = 50f;	// The amount of thrust applied to the ship.
+	[SerializeField] float rcsThrust = 200f;	// The amount of force applied to the rotations.
 	public bool isThrusting = false;
+
+	[Header("Destroy Settings")]
+	//[SerializeField] float breakupForce = 20f;
+	//[SerializeField] float breakupForceRadius = 5f;
+	[SerializeField] GameObject explosionVFX;
+	[SerializeField] AudioClip explosionSFX;
+	[SerializeField] float explosionVolume = 1f;
 
 	private Rigidbody rBody;
 	private AudioSource aSource;
@@ -17,9 +30,9 @@ public class Rocket_Input : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rBody = GetComponent<Rigidbody>();
-		aSource = GetComponent<AudioSource>();
-		startVolume = aSource.volume;
+		rBody = GetComponent<Rigidbody>();	// Reference to ships RigidBody.
+		aSource = GetComponent<AudioSource>(); // Reference to the ships AudioSource.
+		startVolume = aSource.volume;	// added to allow fading thruster sound
 	}
 	
 	// Update is called once per frame
@@ -28,45 +41,98 @@ public class Rocket_Input : MonoBehaviour {
 		RotateShip();
 	}
 
+	#region Ship Input
 	void RotateShip(){
 
-		rBody.freezeRotation = true; // take manual control of rotation
+		rBody.freezeRotation = true; // Take manual control of rotation.
+		float rotationThisFrame = rcsThrust * Time.deltaTime; // Calculates the frame independant rotations.
 
 		if(Input.GetKey(KeyCode.A)){
-			 
-			transform.Rotate(new Vector3 (0,0,RotationSpeed * Time.deltaTime));
-			//transform.Rotate(Vector3.forward);
-
+			transform.Rotate(Vector3.forward * rotationThisFrame); // Rotate the object counter clockwise.
 		} else if (Input.GetKey(KeyCode.D)){
-
-			transform.Rotate(new Vector3 (0,0,-RotationSpeed * Time.deltaTime));
-			//transform.Rotate(-Vector3.forward);
-
+			transform.Rotate(-Vector3.forward * rotationThisFrame); // Rotate the object clockwise.
 		}
 
-		rBody.freezeRotation = false; // resume physics control of rotation
+		rBody.freezeRotation = false; // Resume physics control of rotation.
 	}
 
 	void Thrust(){
-		
+
 		if (Input.GetKey(KeyCode.Space)) {
-			
-			rBody.AddRelativeForce(new Vector3(0, ThrustForce, 0));
-			//rBody.AddRelativeForce(Vector3.up);
+			rBody.AddRelativeForce(Vector3.up * mainThrust); // Moves the rocket on the y axis relative to its local position.
 			isThrusting = true;
 			if(!aSource.isPlaying){
-				aSource.volume = startVolume;
+				aSource.volume = startVolume; // added to allow fading thruster sound
 				aSource.Play();
 			}
 		}
 
 		else {
-			//TODO: use a coroutine or invoke to fade the sound out when thrust stops
+			//TODO: Try to use a coroutine or invoke to fade the thruster sound when thrust stops.
 			isThrusting = false;
 			if(aSource.isPlaying){
 				aSource.Stop();
 			}
 		}
 	}
+	#endregion	
+
+	#region Collision Control
+	void OnCollisionEnter(Collision collision){
 		
+		switch (collision.gameObject.tag){
+			case "Friendly":
+				print("Friendly");
+				//TODO: do nothing remove later
+				break;
+			default:
+				DestroyPlayer();
+				break;
+		}
+	}
+
+	void OnTriggerEnter(Collider collider){
+		// TODO: These can be broken up and added to a pickup script
+		// and added to the item's prefab instead of in the input script.
+		switch (collider.gameObject.tag){
+			case "PowerUp":
+				print("Powerup Collected");
+				// TODO: increase shield
+				break;
+			case "Fuel":
+				print("Fuel Added");
+				//TODO: increase fuel
+				break;
+			case "Coin":
+				print("Coin Collected");
+				//TODO: increase coin total
+				break;
+			default:
+				print("No Tag Set on gameObject");
+				break;
+		}
+
+		Destroy(collider.gameObject);
+	}
+	#endregion
+
+	#region Destroy Player
+	void DestroyPlayer(){
+		// TODO: Make ship break apart
+		Vector3 currentPos = transform.position;
+		// Play explosion vfx
+		if(explosionVFX != null){
+			Instantiate(explosionVFX, currentPos, Quaternion.identity);
+		}
+		// Play Explosion sound
+		if(explosionSFX != null){
+			aSource.clip = explosionSFX;
+			aSource.volume = explosionVolume;
+			aSource.Play();
+		}
+		// Destroy player
+		Destroy(gameObject);
+	}
+	#endregion
+
 }
